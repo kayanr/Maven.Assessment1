29a90a56a945e3096f4081799d95721757de2fc5
val <valfragier@gmail.com>
2019-10-28 09:25:17 -0400

    Complete BasicArrayUtils

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicArrayUtils.java
@@ -9,7 +9,8 @@ public class BasicArrayUtils {
      * @return the first element in the array
      */
     public static String getFirstElement(String[] stringArray) {
-        return null;
+
+        return stringArray[0];
     }
 
     /**
@@ -17,7 +18,8 @@ public class BasicArrayUtils {
      * @return the second element in the array
      */
     public static String getSecondElement(String[] stringArray) {
-        return null;
+
+        return stringArray[1];
     }
 
     /**
@@ -25,14 +27,17 @@ public class BasicArrayUtils {
      * @return the last element in the array
      */
     public static String getLastElement(String[] stringArray) {
-        return null;
-    }
+
+
+        return stringArray[2];
+    }git 
 
     /**
      * @param stringArray an array of String objects
      * @return the second to last element in the array
      */
     public static String getSecondToLastElement(String[] stringArray) {
-        return null;
+
+        return stringArray[2];
     }
 }
