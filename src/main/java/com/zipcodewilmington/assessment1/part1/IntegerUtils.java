430c15ee7560f327fcdb2f3949941e7eb52dc342
val <valfragier@gmail.com>
2019-10-28 10:33:59 -0400

    Complete IntegerUtils

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
@@ -11,7 +11,8 @@ public class IntegerUtils {
      * @return the sum of all integers between 0 and not including `n`
      */
     public static Integer getSumOfN(Integer n) {
-        return null;
+        if (n < 10) ;
+        return (n * (n + 1) / 2);
     }
 
     /**
@@ -19,14 +20,22 @@ public class IntegerUtils {
      * @return the product of all integers between 0 and not including `n`
      */
     public static Integer getProductOfN(Integer n) {
-        return null;
+        if (n == 1) return 1;
+        return n * getProductOfN(n-1);
     }
 
-    /**
-     * @param val integer value input by client
-     * @return integer with identical digits in the reverse order
-     */
-    public static Integer reverseDigits(Integer val) {
-        return null;
-    }
+        /**
+         * @param val integer value input by client
+         * @return integer with identical digits in the reverse order
+         */
+        public static Integer reverseDigits (Integer val){
+            int rev_num = 0;
+            while(val > 0)
+            {
+                rev_num = rev_num * 10 + val % 10;
+                val = val / 10;
+            }
+            return rev_num;
+        }
+
 }
